35fb18a33833c2fc6f197ce62271bbe4928b7f6e
Kshitij <pawarkshitij888@gmail.com>
2021-10-04 17:44:32 +0530

    fix deployment errors

--- seeds/createcamps.js
+++ seeds/createcamps.js
@@ -5,7 +5,7 @@ const cities = require('./citylist');
 const {descriptors, places} = require('./campdatagen');
 const findMap = require('../utils/findMap');
 
-mongoose.connect('mongodb://localhost:27017/campApp');
+mongoose.connect("mongodb+srv://admin-kshitij:5i3Uhfj16RBZIZKF@campreview.kynsx.mongodb.net/campreview?retryWrites=true&w=majority");
 
 if(process.env.NODE_ENV !== "production"){
     require('dotenv').config();
@@ -30,9 +30,9 @@ const seed = async() => {
             description: lorem,
             price: Math.floor(Math.random()*40)+9,
             user: '6149db1f87c1f52124611cc8',
-            image: [{url : "https://res.cloudinary.com/dmyr4hycj/image/upload/v1632487320/CampReview/js1czchiuglgjc04wsqt.jpg", 
-            filename : "CampReview/js1czchiuglgjc04wsqt" }, 
-            { url : "https://res.cloudinary.com/dmyr4hycj/image/upload/v1632487320/CampReview/ek4xesbiabttws1cl2r8.webp", filename : "CampReview/ek4xesbiabttws1cl2r8" } ],
+            image: [{url : "https://res.cloudinary.com/dmyr4hycj/image/upload/v1632488618/CampReview/spny8ay5e9vjooplhyeo.jpg", 
+            filename : "CampReview/spny8ay5e9vjooplhyeo" }, 
+            { url : "https://res.cloudinary.com/dmyr4hycj/image/upload/v1632488618/CampReview/yvmqax4kktinyiybdhvh.webp", filename : "CampReview/yvmqax4kktinyiybdhvh" } ],
             map: {type: "Point", coordinates: [randCity.longitude, randCity.latitude]}
             })
          
